a2935a7a6526a3d5c0d5c6d89541a26dbbdeda4b
Thomas von Deyen <tvdeyen@gmail.com>
2013-11-14 15:04:44 +0100

    Adds whitelist option.
    You can now set a whitelist of file paths that should be copied to a non digest version.
    
    ## Usage example
    
    Add an initializer into `config/initializers` folder:
    
    ```
        # config/initializers/non_digest_assets.rb
        NonStupidDigestAssets.whitelist = ['tinymce/']
    ```

--- lib/non-stupid-digest-assets.rb
+++ lib/non-stupid-digest-assets.rb
@@ -1,9 +1,22 @@
+module NonStupidDigestAssets
+  mattr_accessor :whitelist
+  @@whitelist = []
+end
+
 module Sprockets
   class Manifest
     def compile_with_non_digest *args
       compile_without_non_digest *args
-
-      files.each do |(digest_path, info)|
+      if NonStupidDigestAssets.whitelist.empty?
+        files_to_copy = files
+      else
+        files_to_copy = files.select do |file, info|
+          !NonStupidDigestAssets.whitelist.detect do |item|
+            info['logical_path'] =~ /#{item}/
+          end.nil?
+        end
+      end
+      files_to_copy.each do |(digest_path, info)|
         full_digest_path = File.join dir, digest_path
         full_digest_gz_path = "#{full_digest_path}.gz"
         full_non_digest_path = File.join dir, info['logical_path']
